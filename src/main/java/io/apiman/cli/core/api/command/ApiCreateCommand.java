msavy/apiman-cli

Switch to JCommander to allow more flexible architecture.
- Descriptions for each subcommand.

- Custom Git-style #usage/#help to avoid information overload.

- Altered initialisation to allow JCommander to use utilise
  subcommand and help functionality more extensively.

- Same Guice injector for all instances.

src/main/java/io/apiman/cli/core/api/command/ApiCreateCommand.java
@@ -16,18 +16,19 @@
 
 package io.apiman.cli.core.api.command;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 import com.google.common.collect.Lists;
 import io.apiman.cli.core.api.ApiMixin;
+import io.apiman.cli.core.api.VersionAgnosticApi;
 import io.apiman.cli.core.api.model.Api;
 import io.apiman.cli.core.api.model.ApiConfig;
 import io.apiman.cli.core.api.model.ApiGateway;
-import io.apiman.cli.core.api.VersionAgnosticApi;
 import io.apiman.cli.exception.CommandException;
 import io.apiman.cli.management.ManagementApiUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.Option;
 
 import java.text.MessageFormat;
 
@@ -36,28 +37,29 @@ import java.text.MessageFormat;
  *
  * @author Pete Cornish {@literal <outofcoffee@gmail.com>}
  */
+@Parameters(commandNames = "Create an API")
 public class ApiCreateCommand extends AbstractApiCommand implements ApiMixin {
     private static final Logger LOGGER = LogManager.getLogger(ApiCreateCommand.class);
 
-    @Option(name = "--name", aliases = {"-n"}, usage = "API name", required = true)
+    @Parameter(names = {"--name", "-n"}, description = "API name", required = true)
     private String name;
 
-    @Option(name = "--description", aliases = {"-d"}, usage = "Description")
+    @Parameter(names = {"--description", "-d"}, description = "Description")
     private String description;
 
-    @Option(name = "--initialVersion", aliases = {"-v"}, usage = "Initial version", required = true)
+    @Parameter(names = {"--initialVersion", "-v"}, description = "Initial version", required = true)
     private String initialVersion;
 
-    @Option(name = "--endpoint", aliases = {"-e"}, usage = "Endpoint", required = true)
+    @Parameter(names = {"--endpoint", "-e"}, description = "Endpoint", required = true)
     private String endpoint;
 
-    @Option(name = "--endpointType", aliases = {"-t"}, usage = "Endpoint type")
+    @Parameter(names = {"--endpointType", "-t"}, description = "Endpoint type")
     private String endpointType = "rest";
 
-    @Option(name = "--public", aliases = {"-p"}, usage = "Public API", required = true)
+    @Parameter(names = {"--public", "-p"}, description = "Public API", required = true)
     private boolean publicApi;
 
-    @Option(name = "--gateway", aliases = {"-g"}, usage = "Gateway")
+    @Parameter(names = {"--gateway", "-g"}, description = "Gateway")
     private String gateway = "TheGateway";
 
     @Override
@@ -66,7 +68,7 @@ public class ApiCreateCommand extends AbstractApiCommand implements ApiMixin {
     }
 
     @Override
-    public void performAction(CmdLineParser parser) throws CommandException {
+    public void performAction(JCommander parser) throws CommandException {
         LOGGER.debug("Creating {}", this::getModelName);
 
         final Api api = new Api(
@@ -81,7 +83,7 @@ public class ApiCreateCommand extends AbstractApiCommand implements ApiMixin {
                 Lists.newArrayList(new ApiGateway(gateway)));
 
         // create
-        final VersionAgnosticApi apiClient = buildServerApiClient(VersionAgnosticApi.class, serverVersion);
+        final VersionAgnosticApi apiClient = getManagerConfig().buildServerApiClient(VersionAgnosticApi.class, serverVersion);
         ManagementApiUtil.invokeAndCheckResponse(() -> apiClient.create(orgName, api));
 
         // configure

src/main/java/io/apiman/cli/core/api/command/ApiDefinitionCommand.java
@@ -16,18 +16,17 @@
 
 package io.apiman.cli.core.api.command;
 
-import com.google.common.collect.Lists;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 import com.google.common.io.CharStreams;
 import io.apiman.cli.core.api.ApiMixin;
 import io.apiman.cli.core.api.VersionAgnosticApi;
-import io.apiman.cli.core.api.model.*;
 import io.apiman.cli.exception.CommandException;
 import io.apiman.cli.exception.ExitWithCodeException;
 import io.apiman.cli.management.ManagementApiUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.Option;
 import retrofit.mime.TypedString;
 
 import java.io.IOException;
@@ -42,22 +41,23 @@ import java.text.MessageFormat;
  *
  * @author Raleigh Pickard {@literal <raleigh.pickard@gmail.com>}
  */
+@Parameters(commandNames = "Create an API using declarative definition")
 public class ApiDefinitionCommand extends AbstractApiCommand implements ApiMixin {
     private static final Logger LOGGER = LogManager.getLogger(ApiDefinitionCommand.class);
 
-    @Option(name = "--name", aliases = {"-n"}, usage = "API name", required = true)
+    @Parameter(names = { "--name", "-n"}, description = "API name", required = true)
     private String name;
 
-    @Option(name = "--version", aliases = {"-v"}, usage = "API version", required = true)
+    @Parameter(names = { "--version", "-v"}, description = "API version", required = true)
     private String version;
 
-    @Option(name = "--definitionStdIn", aliases = {"-i"}, usage = "Read API definition from STDIN", forbids = "-f")
+    @Parameter(names = { "--definitionStdIn", "-i"}, description = "Read API definition from STDIN") // , forbids = "-f"
     private boolean definitionStdIn;
 
-    @Option(name = "--definitionFile", aliases = {"-f"}, usage = "API definition configuration file", forbids = "-i")
+    @Parameter(names = { "--definitionFile", "-f"}, description = "API definition configuration file") // , forbids = "-i")
     private Path definitionFile;
 
-    @Option(name = "--definitionType", aliases = {"-t"}, usage = "Endpoint", required = true)
+    @Parameter(names = { "--definitionType", "-t"}, description = "Endpoint", required = true)
     private String definitionType = "application/json";
 
     @Override
@@ -66,7 +66,7 @@ public class ApiDefinitionCommand extends AbstractApiCommand implements ApiMixin
     }
 
     @Override
-    public void performAction(CmdLineParser parser) throws CommandException {
+    public void performAction(JCommander parser) throws CommandException {
         if (!definitionStdIn && null == definitionFile) {
             throw new ExitWithCodeException(1, "API definition must be provided", true);
         }
@@ -83,6 +83,6 @@ public class ApiDefinitionCommand extends AbstractApiCommand implements ApiMixin
         LOGGER.debug("Adding definition to API '{}' with contents: {}", this::getModelName, () -> definition);
 
         ManagementApiUtil.invokeAndCheckResponse(() ->
-                buildServerApiClient(VersionAgnosticApi.class, serverVersion).setDefinition(orgName, name, version, definitionType, new TypedString(definition)));
+                getManagerConfig().buildServerApiClient(VersionAgnosticApi.class, serverVersion).setDefinition(orgName, name, version, definitionType, new TypedString(definition)));
     }
 }

src/main/java/io/apiman/cli/core/api/command/ApiListCommand.java
@@ -16,15 +16,16 @@
 
 package io.apiman.cli.core.api.command;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameters;
 import io.apiman.cli.core.api.ApiMixin;
-import io.apiman.cli.core.api.model.Api;
 import io.apiman.cli.core.api.VersionAgnosticApi;
+import io.apiman.cli.core.api.model.Api;
 import io.apiman.cli.exception.CommandException;
 import io.apiman.cli.util.LogUtil;
 import io.apiman.cli.util.MappingUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.kohsuke.args4j.CmdLineParser;
 
 import java.text.MessageFormat;
 import java.util.List;
@@ -34,6 +35,7 @@ import java.util.List;
  *
  * @author Pete Cornish {@literal <outofcoffee@gmail.com>}
  */
+@Parameters(commandNames = "List APIs")
 public class ApiListCommand extends AbstractApiCommand implements ApiMixin {
     private static final Logger LOGGER = LogManager.getLogger(ApiListCommand.class);
 
@@ -43,10 +45,10 @@ public class ApiListCommand extends AbstractApiCommand implements ApiMixin {
     }
 
     @Override
-    public void performAction(CmdLineParser parser) throws CommandException {
+    public void performAction(JCommander parser) throws CommandException {
         LOGGER.debug("Listing {}", this::getModelName);
 
-        final List<Api> apis = buildServerApiClient(VersionAgnosticApi.class, serverVersion).list(orgName);
+        final List<Api> apis = getManagerConfig().buildServerApiClient(VersionAgnosticApi.class, serverVersion).list(orgName);
         LogUtil.OUTPUT.info(MappingUtil.safeWriteValueAsJson(apis));
     }
 }

src/main/java/io/apiman/cli/core/api/command/ApiPolicyAddCommand.java
@@ -16,17 +16,18 @@
 
 package io.apiman.cli.core.api.command;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 import com.google.common.io.CharStreams;
 import io.apiman.cli.core.api.ApiMixin;
-import io.apiman.cli.core.api.model.ApiPolicy;
 import io.apiman.cli.core.api.VersionAgnosticApi;
+import io.apiman.cli.core.api.model.ApiPolicy;
 import io.apiman.cli.exception.CommandException;
 import io.apiman.cli.exception.ExitWithCodeException;
 import io.apiman.cli.management.ManagementApiUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.Option;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -40,22 +41,23 @@ import java.text.MessageFormat;
  *
  * @author Pete Cornish {@literal <outofcoffee@gmail.com>}
  */
+@Parameters(commandNames = "Add policy to API")
 public class ApiPolicyAddCommand extends AbstractApiCommand implements ApiMixin {
     private static final Logger LOGGER = LogManager.getLogger(ApiPolicyAddCommand.class);
 
-    @Option(name = "--name", aliases = {"-n"}, usage = "API name", required = true)
+    @Parameter(names = { "--name", "-n" }, description = "API name", required = true)
     private String name;
 
-    @Option(name = "--version", aliases = {"-v"}, usage = "API version", required = true)
+    @Parameter(names = { "--version", "-v" }, description = "API version", required = true)
     private String version;
 
-    @Option(name = "--policyName", aliases = {"-p"}, usage = "Policy name", required = true)
+    @Parameter(names = { "--policyName", "-p" }, description = "Policy name", required = true)
     private String policyName;
 
-    @Option(name = "--configStdIn", aliases = {"-i"}, usage = "Read policy configuration from STDIN", forbids = "-f")
+    @Parameter(names = { "--configStdIn", "-i" }, description = "Read policy configuration from STDIN") // TODO forbids -f
     private boolean configStdIn;
 
-    @Option(name = "--configFile", aliases = {"-f"}, usage = "Policy configuration file", forbids = "-i")
+    @Parameter(names = { "--configFile", "-f" }, description = "Policy configuration file") // TODO forbids i
     private Path configFile;
 
     @Override
@@ -64,7 +66,7 @@ public class ApiPolicyAddCommand extends AbstractApiCommand implements ApiMixin 
     }
 
     @Override
-    public void performAction(CmdLineParser parser) throws CommandException {
+    public void performAction(JCommander parser) throws CommandException {
         if (!configStdIn && null == configFile) {
             throw new ExitWithCodeException(1, "Policy configuration must be provided", true);
         }
@@ -85,6 +87,6 @@ public class ApiPolicyAddCommand extends AbstractApiCommand implements ApiMixin 
         apiPolicy.setDefinitionId(policyName);
 
         ManagementApiUtil.invokeAndCheckResponse(() ->
-                buildServerApiClient(VersionAgnosticApi.class, serverVersion).addPolicy(orgName, name, version, apiPolicy));
+                getManagerConfig().buildServerApiClient(VersionAgnosticApi.class, serverVersion).addPolicy(orgName, name, version, apiPolicy));
     }
 }

src/main/java/io/apiman/cli/core/api/command/ApiPublishCommand.java
@@ -16,14 +16,15 @@
 
 package io.apiman.cli.core.api.command;
 
-import io.apiman.cli.core.common.util.ServerActionUtil;
-import io.apiman.cli.exception.CommandException;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 import io.apiman.cli.core.api.ApiMixin;
 import io.apiman.cli.core.common.ActionApi;
+import io.apiman.cli.core.common.util.ServerActionUtil;
+import io.apiman.cli.exception.CommandException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.Option;
 
 import java.text.MessageFormat;
 
@@ -32,13 +33,14 @@ import java.text.MessageFormat;
  *
  * @author Pete Cornish {@literal <outofcoffee@gmail.com>}
  */
+@Parameters(commandNames = "Publish API")
 public class ApiPublishCommand extends AbstractApiCommand implements ApiMixin {
     private static final Logger LOGGER = LogManager.getLogger(ApiPublishCommand.class);
 
-    @Option(name = "--name", aliases = {"-n"}, usage = "API name", required = true)
+    @Parameter(names = { "--name", "-n"}, description = "API name", required = true)
     private String name;
 
-    @Option(name = "--version", aliases = {"-v"}, usage = "API version", required = true)
+    @Parameter(names = { "--version", "-v"}, description = "API version", required = true)
     private String version;
 
     @Override
@@ -47,9 +49,9 @@ public class ApiPublishCommand extends AbstractApiCommand implements ApiMixin {
     }
 
     @Override
-    public void performAction(CmdLineParser parser) throws CommandException {
+    public void performAction(JCommander parser) throws CommandException {
         LOGGER.debug("Publishing {}", this::getModelName);
-        ServerActionUtil.publishApi(orgName, name, version, serverVersion, buildServerApiClient(ActionApi.class));
+        ServerActionUtil.publishApi(orgName, name, version, serverVersion, getManagerConfig().buildServerApiClient(ActionApi.class));
     }
 
 }

msavy/apiman-cli-develop

src/main/java/io/apiman/cli/core/api/command/ApiCreateCommand.java
@@ -16,18 +16,19 @@
 
 package io.apiman.cli.core.api.command;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 import com.google.common.collect.Lists;
 import io.apiman.cli.core.api.ApiMixin;
+import io.apiman.cli.core.api.VersionAgnosticApi;
 import io.apiman.cli.core.api.model.Api;
 import io.apiman.cli.core.api.model.ApiConfig;
 import io.apiman.cli.core.api.model.ApiGateway;
-import io.apiman.cli.core.api.VersionAgnosticApi;
 import io.apiman.cli.exception.CommandException;
 import io.apiman.cli.management.ManagementApiUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.Option;
 
 import java.text.MessageFormat;
 
@@ -36,28 +37,29 @@ import java.text.MessageFormat;
  *
  * @author Pete Cornish {@literal <outofcoffee@gmail.com>}
  */
+@Parameters(commandDescription = "Create an API")
 public class ApiCreateCommand extends AbstractApiCommand implements ApiMixin {
     private static final Logger LOGGER = LogManager.getLogger(ApiCreateCommand.class);
 
-    @Option(name = "--name", aliases = {"-n"}, usage = "API name", required = true)
+    @Parameter(names = {"--name", "-n"}, description = "API name", required = true)
     private String name;
 
-    @Option(name = "--description", aliases = {"-d"}, usage = "Description")
+    @Parameter(names = {"--description", "-d"}, description = "Description")
     private String description;
 
-    @Option(name = "--initialVersion", aliases = {"-v"}, usage = "Initial version", required = true)
+    @Parameter(names = {"--initialVersion", "-v"}, description = "Initial version", required = true)
     private String initialVersion;
 
-    @Option(name = "--endpoint", aliases = {"-e"}, usage = "Endpoint", required = true)
+    @Parameter(names = {"--endpoint", "-e"}, description = "Endpoint", required = true)
     private String endpoint;
 
-    @Option(name = "--endpointType", aliases = {"-t"}, usage = "Endpoint type")
+    @Parameter(names = {"--endpointType", "-t"}, description = "Endpoint type")
     private String endpointType = "rest";
 
-    @Option(name = "--public", aliases = {"-p"}, usage = "Public API", required = true)
+    @Parameter(names = {"--public", "-p"}, description = "Public API", required = true)
     private boolean publicApi;
 
-    @Option(name = "--gateway", aliases = {"-g"}, usage = "Gateway")
+    @Parameter(names = {"--gateway", "-g"}, description = "Gateway")
     private String gateway = "TheGateway";
 
     @Override
@@ -66,7 +68,7 @@ public class ApiCreateCommand extends AbstractApiCommand implements ApiMixin {
     }
 
     @Override
-    public void performAction(CmdLineParser parser) throws CommandException {
+    public void performAction(JCommander parser) throws CommandException {
         LOGGER.debug("Creating {}", this::getModelName);
 
         final Api api = new Api(
@@ -81,7 +83,7 @@ public class ApiCreateCommand extends AbstractApiCommand implements ApiMixin {
                 Lists.newArrayList(new ApiGateway(gateway)));
 
         // create
-        final VersionAgnosticApi apiClient = buildServerApiClient(VersionAgnosticApi.class, serverVersion);
+        final VersionAgnosticApi apiClient = getManagerConfig().buildServerApiClient(VersionAgnosticApi.class, serverVersion);
         ManagementApiUtil.invokeAndCheckResponse(() -> apiClient.create(orgName, api));
 
         // configure

src/main/java/io/apiman/cli/core/api/command/ApiDefinitionCommand.java
@@ -16,18 +16,17 @@
 
 package io.apiman.cli.core.api.command;
 
-import com.google.common.collect.Lists;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 import com.google.common.io.CharStreams;
 import io.apiman.cli.core.api.ApiMixin;
 import io.apiman.cli.core.api.VersionAgnosticApi;
-import io.apiman.cli.core.api.model.*;
 import io.apiman.cli.exception.CommandException;
 import io.apiman.cli.exception.ExitWithCodeException;
 import io.apiman.cli.management.ManagementApiUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.Option;
 import retrofit.mime.TypedString;
 
 import java.io.IOException;
@@ -42,22 +41,23 @@ import java.text.MessageFormat;
  *
  * @author Raleigh Pickard {@literal <raleigh.pickard@gmail.com>}
  */
+@Parameters(commandDescription = "Create an API using declarative definition")
 public class ApiDefinitionCommand extends AbstractApiCommand implements ApiMixin {
     private static final Logger LOGGER = LogManager.getLogger(ApiDefinitionCommand.class);
 
-    @Option(name = "--name", aliases = {"-n"}, usage = "API name", required = true)
+    @Parameter(names = { "--name", "-n"}, description = "API name", required = true)
     private String name;
 
-    @Option(name = "--version", aliases = {"-v"}, usage = "API version", required = true)
+    @Parameter(names = { "--version", "-v"}, description = "API version", required = true)
     private String version;
 
-    @Option(name = "--definitionStdIn", aliases = {"-i"}, usage = "Read API definition from STDIN", forbids = "-f")
+    @Parameter(names = { "--definitionStdIn", "-i"}, description = "Read API definition from STDIN") // , forbids = "-f"
     private boolean definitionStdIn;
 
-    @Option(name = "--definitionFile", aliases = {"-f"}, usage = "API definition configuration file", forbids = "-i")
+    @Parameter(names = { "--definitionFile", "-f"}, description = "API definition configuration file") // , forbids = "-i")
     private Path definitionFile;
 
-    @Option(name = "--definitionType", aliases = {"-t"}, usage = "Endpoint", required = true)
+    @Parameter(names = { "--definitionType", "-t"}, description = "Endpoint", required = true)
     private String definitionType = "application/json";
 
     @Override
@@ -66,7 +66,7 @@ public class ApiDefinitionCommand extends AbstractApiCommand implements ApiMixin
     }
 
     @Override
-    public void performAction(CmdLineParser parser) throws CommandException {
+    public void performAction(JCommander parser) throws CommandException {
         if (!definitionStdIn && null == definitionFile) {
             throw new ExitWithCodeException(1, "API definition must be provided", true);
         }
@@ -83,6 +83,6 @@ public class ApiDefinitionCommand extends AbstractApiCommand implements ApiMixin
         LOGGER.debug("Adding definition to API '{}' with contents: {}", this::getModelName, () -> definition);
 
         ManagementApiUtil.invokeAndCheckResponse(() ->
-                buildServerApiClient(VersionAgnosticApi.class, serverVersion).setDefinition(orgName, name, version, definitionType, new TypedString(definition)));
+                getManagerConfig().buildServerApiClient(VersionAgnosticApi.class, serverVersion).setDefinition(orgName, name, version, definitionType, new TypedString(definition)));
     }
 }

src/main/java/io/apiman/cli/core/api/command/ApiListCommand.java
@@ -16,15 +16,16 @@
 
 package io.apiman.cli.core.api.command;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameters;
 import io.apiman.cli.core.api.ApiMixin;
-import io.apiman.cli.core.api.model.Api;
 import io.apiman.cli.core.api.VersionAgnosticApi;
+import io.apiman.cli.core.api.model.Api;
 import io.apiman.cli.exception.CommandException;
 import io.apiman.cli.util.LogUtil;
 import io.apiman.cli.util.MappingUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.kohsuke.args4j.CmdLineParser;
 
 import java.text.MessageFormat;
 import java.util.List;
@@ -34,6 +35,7 @@ import java.util.List;
  *
  * @author Pete Cornish {@literal <outofcoffee@gmail.com>}
  */
+@Parameters(commandDescription = "List APIs")
 public class ApiListCommand extends AbstractApiCommand implements ApiMixin {
     private static final Logger LOGGER = LogManager.getLogger(ApiListCommand.class);
 
@@ -43,10 +45,10 @@ public class ApiListCommand extends AbstractApiCommand implements ApiMixin {
     }
 
     @Override
-    public void performAction(CmdLineParser parser) throws CommandException {
+    public void performAction(JCommander parser) throws CommandException {
         LOGGER.debug("Listing {}", this::getModelName);
 
-        final List<Api> apis = buildServerApiClient(VersionAgnosticApi.class, serverVersion).list(orgName);
+        final List<Api> apis = getManagerConfig().buildServerApiClient(VersionAgnosticApi.class, serverVersion).list(orgName);
         LogUtil.OUTPUT.info(MappingUtil.safeWriteValueAsJson(apis));
     }
 }

src/main/java/io/apiman/cli/core/api/command/ApiPolicyAddCommand.java
@@ -16,17 +16,18 @@
 
 package io.apiman.cli.core.api.command;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 import com.google.common.io.CharStreams;
 import io.apiman.cli.core.api.ApiMixin;
-import io.apiman.cli.core.api.model.ApiPolicy;
 import io.apiman.cli.core.api.VersionAgnosticApi;
+import io.apiman.cli.core.api.model.ApiPolicy;
 import io.apiman.cli.exception.CommandException;
 import io.apiman.cli.exception.ExitWithCodeException;
 import io.apiman.cli.management.ManagementApiUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.Option;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -40,22 +41,23 @@ import java.text.MessageFormat;
  *
  * @author Pete Cornish {@literal <outofcoffee@gmail.com>}
  */
+@Parameters(commandDescription = "Add policy to API")
 public class ApiPolicyAddCommand extends AbstractApiCommand implements ApiMixin {
     private static final Logger LOGGER = LogManager.getLogger(ApiPolicyAddCommand.class);
 
-    @Option(name = "--name", aliases = {"-n"}, usage = "API name", required = true)
+    @Parameter(names = { "--name", "-n" }, description = "API name", required = true)
     private String name;
 
-    @Option(name = "--version", aliases = {"-v"}, usage = "API version", required = true)
+    @Parameter(names = { "--version", "-v" }, description = "API version", required = true)
     private String version;
 
-    @Option(name = "--policyName", aliases = {"-p"}, usage = "Policy name", required = true)
+    @Parameter(names = { "--policyName", "-p" }, description = "Policy name", required = true)
     private String policyName;
 
-    @Option(name = "--configStdIn", aliases = {"-i"}, usage = "Read policy configuration from STDIN", forbids = "-f")
+    @Parameter(names = { "--configStdIn", "-i" }, description = "Read policy configuration from STDIN") // TODO forbids -f
     private boolean configStdIn;
 
-    @Option(name = "--configFile", aliases = {"-f"}, usage = "Policy configuration file", forbids = "-i")
+    @Parameter(names = { "--configFile", "-f" }, description = "Policy configuration file") // TODO forbids i
     private Path configFile;
 
     @Override
@@ -64,7 +66,7 @@ public class ApiPolicyAddCommand extends AbstractApiCommand implements ApiMixin 
     }
 
     @Override
-    public void performAction(CmdLineParser parser) throws CommandException {
+    public void performAction(JCommander parser) throws CommandException {
         if (!configStdIn && null == configFile) {
             throw new ExitWithCodeException(1, "Policy configuration must be provided", true);
         }
@@ -85,6 +87,6 @@ public class ApiPolicyAddCommand extends AbstractApiCommand implements ApiMixin 
         apiPolicy.setDefinitionId(policyName);
 
         ManagementApiUtil.invokeAndCheckResponse(() ->
-                buildServerApiClient(VersionAgnosticApi.class, serverVersion).addPolicy(orgName, name, version, apiPolicy));
+                getManagerConfig().buildServerApiClient(VersionAgnosticApi.class, serverVersion).addPolicy(orgName, name, version, apiPolicy));
     }
 }

src/main/java/io/apiman/cli/core/api/command/ApiPublishCommand.java
@@ -16,14 +16,15 @@
 
 package io.apiman.cli.core.api.command;
 
-import io.apiman.cli.core.common.util.ServerActionUtil;
-import io.apiman.cli.exception.CommandException;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 import io.apiman.cli.core.api.ApiMixin;
 import io.apiman.cli.core.common.ActionApi;
+import io.apiman.cli.core.common.util.ServerActionUtil;
+import io.apiman.cli.exception.CommandException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.Option;
 
 import java.text.MessageFormat;
 
@@ -32,13 +33,14 @@ import java.text.MessageFormat;
  *
  * @author Pete Cornish {@literal <outofcoffee@gmail.com>}
  */
+@Parameters(commandDescription = "Publish API")
 public class ApiPublishCommand extends AbstractApiCommand implements ApiMixin {
     private static final Logger LOGGER = LogManager.getLogger(ApiPublishCommand.class);
 
-    @Option(name = "--name", aliases = {"-n"}, usage = "API name", required = true)
+    @Parameter(names = { "--name", "-n"}, description = "API name", required = true)
     private String name;
 
-    @Option(name = "--version", aliases = {"-v"}, usage = "API version", required = true)
+    @Parameter(names = { "--version", "-v"}, description = "API version", required = true)
     private String version;
 
     @Override
@@ -47,9 +49,9 @@ public class ApiPublishCommand extends AbstractApiCommand implements ApiMixin {
     }
 
     @Override
-    public void performAction(CmdLineParser parser) throws CommandException {
+    public void performAction(JCommander parser) throws CommandException {
         LOGGER.debug("Publishing {}", this::getModelName);
-        ServerActionUtil.publishApi(orgName, name, version, serverVersion, buildServerApiClient(ActionApi.class));
+        ServerActionUtil.publishApi(orgName, name, version, serverVersion, getManagerConfig().buildServerApiClient(ActionApi.class));
     }
 
 }